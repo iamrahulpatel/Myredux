bdb278697c6ff145978f3667d6a862956bede107
iamrahulpatel <rahul99289@gmail.com>
2021-05-26 15:56:20 +0530

    commits resolved

--- src/routing/AppNavigation.js
+++ src/routing/AppNavigation.js
@@ -1,6 +1,7 @@
+
 import 'react-native-gesture-handler';
-import React,{useEffect} from 'react';
-import {Linking} from "react-native";
+import React, { useEffect } from 'react';
+import { Linking } from "react-native";
 import Signup from '../components/AuthScreen/Signup';
 import Profile from '../components/AuthScreen/Profile';
 import EditProfile from '../components/AuthScreen/EditProfile';
@@ -15,11 +16,60 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useAppState } from '../components/common/useAppState';
 
-
 const Stack = createStackNavigator();
 
 const AppNavigation = () => {
 
+  const currentAppState = useAppState()
+
+  const deepLink = {
+    prefixes: ["my-redux://", "https://my-redux.com/app"],
+    config: {
+      Profile: 'profile',
+      MyMap: 'maps',
+      DeepLink: {
+        path: 'deep/:id',
+        params: {
+          id: null
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    Linking.addEventListener("url", ({ url }) => {
+      // console.log("listener")
+      if (url) {
+        // letarr = url.split("/")
+        // idParam = arr[arr.length - 1]
+        // dummyDeepLinkedUrl = url;
+        // if (idParam.includes("_")) {
+        // idParam = idParam.substring(0, idParam.indexOf("_"))
+        // }
+        // navigateToUrl(url, idParam)
+        console.log("event listener", url)
+      }
+    })
+    if (currentAppState == 'active') {
+      Linking.getInitialURL().then(url => {
+        console.log("second listener", url)
+      });
+    }
+    return (() => {
+      removeLinkingListeners()
+    })
+  }, [])
+
+  const removeLinkingListeners = async () => {
+    Linking.removeAllListeners('url')
+  }
+
+  useEffect(() => {
+    console.log("currentAppState ", currentAppState)
+  }, [currentAppState])
+
+  return (
+    <NavigationContainer linking={deepLink}>
       <Stack.Navigator headerMode={false} initialRouteName={Signup}>
 
         <Stack.Screen name="Signup" component={Signup} />
@@ -29,6 +79,8 @@ const AppNavigation = () => {
         <Stack.Screen name="PostList" component={PostList} />
         <Stack.Screen name="PostDetail" component={PostDetail} />
         <Stack.Screen name="MyMap" component={MyMap} />
+        <Stack.Screen name="DeepLink" component={DeepLink} />
+        <Stack.Screen name="CameraRolling" component={CameraRolling} />
 
       </Stack.Navigator>
     </NavigationContainer>
